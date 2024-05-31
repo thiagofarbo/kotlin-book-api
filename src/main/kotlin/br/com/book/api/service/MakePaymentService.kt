b0719e09c440eaf967486c6b9bb3f2be71f3fa18
Thiago Correa <thiago.correa_ntconsult@prestador.globo>
2024-05-31 15:34:42 +0200

    Created integration to Prometheus and Grafana

--- src/main/kotlin/br/com/book/api/service/MakePaymentService.kt
+++ src/main/kotlin/br/com/book/api/service/MakePaymentService.kt
@@ -7,8 +7,6 @@ import org.springframework.http.HttpHeaders
 import org.springframework.http.MediaType
 import org.springframework.stereotype.Service
 import org.springframework.web.client.RestTemplate
-import org.springframework.web.reactive.function.BodyInserters
-import org.springframework.web.reactive.function.client.WebClient
 
 @Service
 class MakePaymentService(){
